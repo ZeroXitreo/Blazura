a4164cfdc8b2c7689be83c5265b2010738e1d4d5
ZeroXitreo <3112566+ZeroXitreo@users.noreply.github.com>
2025-08-14 17:30:34 +0200

    Add pad component

---
+++ Blazura/Models/Sizing.cs
@@ -0,0 +1,12 @@
+﻿namespace Blazura.Models;
+
+public enum Sizing
+{
+    Xxs, // Extra Extra Small
+    Xs, // Extra Small
+    Sm, // Small
+    Md, // Medium
+    Lg, // Large
+    Xl,  // Extra Large
+    Xxl // Extra Extra Large
+}
