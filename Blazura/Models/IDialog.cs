56c0c41deafc31a2e5b4b413ee6f462181b55281
ZeroXitreo <3112566+ZeroXitreo@users.noreply.github.com>
2026-07-17 19:19:29 +0200

    Move to dialog manager

---
+++ Blazura/Models/IDialog.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace Blazura.Models;
+
+internal interface IDialog : IDisposable
+{
+    public RenderFragment ChildContent { get; set; }
+    public bool IsOpen { get; set; }
+    public void Open();
+    public Task Close();
+}
